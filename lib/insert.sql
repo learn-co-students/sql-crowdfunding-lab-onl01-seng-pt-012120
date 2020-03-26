c99c2e64d7006352de03bf70273511e4fabf291d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-26 22:45:34 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+
+SELECT * FROM users;
+
+-- INSERT INTO users VALUES (1, "A", 25);
+-- INSERT INTO users VALUES (2, "B", 25);
+-- INSERT INTO users VALUES (3, "C", 25);
+-- INSERT INTO users VALUES (4, "D", 25);
+-- INSERT INTO users VALUES (5, "E", 25);
+-- INSERT INTO users VALUES (6, "F", 25);
+-- INSERT INTO users VALUES (7, "G", 25);
+-- INSERT INTO users VALUES (8, "H", 25);
+-- INSERT INTO users VALUES (9, "I", 25);
+-- INSERT INTO users VALUES (10, "J", 25);
+-- INSERT INTO users VALUES (11, "K", 25);
+-- INSERT INTO users VALUES (12, "L", 25);
+-- INSERT INTO users VALUES (13, "M", 25);
+-- INSERT INTO users VALUES (14, "N", 25);
+-- INSERT INTO users VALUES (15, "O", 25);
+-- INSERT INTO users VALUES (16, "P", 25);
+-- INSERT INTO users VALUES (17, "Q", 25);
+-- INSERT INTO users VALUES (18, "R", 25);
+-- INSERT INTO users VALUES (19, "S", 25);
+-- INSERT INTO users VALUES (20, "T", 25);
+--
+-- INSERT INTO projects VALUES (1, "humane_society", "animal_volunteer",
+-- 5,000, 1201, 0331);
