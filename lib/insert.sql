f06e8e6e59c87695f5e170e80a2be92fe73a5a01
Derric Fragosa <fderric@gmail.com>
2020-03-26 17:09:43 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,34 @@
+INSERT INTO projects VALUES(1,'ProjecA', 'Music', 3000, 1/2/20, 1/2/21 );
+INSERT INTO projects VALUES(2,'ProjecB', 'Movie', 10000, 2/2/20, 2/2/21 );
+INSERT INTO projects VALUES(3,'ProjecC', 'Tournament', 5000, 5/2/20, 5/2/21 );
+INSERT INTO projects VALUES(4,'ProjecD', 'Music', 2000, 10/2/20, 10/2/21 );
+INSERT INTO projects VALUES(5,'ProjecE', 'Tournament', 700, 4/2/20, 4/2/21 );
+INSERT INTO projects VALUES(6,'ProjecF', 'Movie', 8000, 11/2/20, 11/2/21 );
+INSERT INTO projects VALUES(7,'ProjecG', 'Fundraiser', 20000, 1/2/20, 3/2/20);
+INSERT INTO projects VALUES(8,'ProjecH', 'Tournament', 2000, 3/2/20, 12/2/20);
+INSERT INTO projects VALUES(9,'ProjecI', 'Music', 7000, 6/2/20, 11/2/20 );
+INSERT INTO projects VALUES(10,'ProjecJ', 'Movie', 15000, 1/2/20, 1/2/21 );
+
+INSERT INTO users VALUES(1,'Sam', 21);
+INSERT INTO users VALUES(2,'Tan', 20);
+INSERT INTO users VALUES(3,'Bam', 34);
+INSERT INTO users VALUES(4,'Cam', 25);
+INSERT INTO users VALUES(5,'Dan', 35);
+INSERT INTO users VALUES(6,'Fam', 33);
+INSERT INTO users VALUES(7,'Gam', 21);
+INSERT INTO users VALUES(8,'Ham', 27);
+INSERT INTO users VALUES(9,'Ian', 15);
+INSERT INTO users VALUES(10,'Jam', 45);
+INSERT INTO users VALUES(11,'Kem', 35);
+INSERT INTO users VALUES(12,'Lam', 36);
+INSERT INTO users VALUES(13,'Mam', 10);
+INSERT INTO users VALUES(14,'Nam', 19);
+INSERT INTO users VALUES(15,'Sam', 28);
+INSERT INTO users VALUES(16,'Omam', 22);
+INSERT INTO users VALUES(17,'Pam', 30);
+INSERT INTO users VALUES(18,'Qam', 40);
+INSERT INTO users VALUES(19,'Ramy', 55);
+INSERT INTO users VALUES(20,'Sam', 65);
+
+
+
