10911a4e0b86b8e313410f932cc964d721ba078d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-26 23:32:13 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,26 +1,48 @@
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
-SELECT * FROM users;
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
--- INSERT INTO users VALUES (1, "A", 25);
--- INSERT INTO users VALUES (2, "B", 25);
--- INSERT INTO users VALUES (3, "C", 25);
--- INSERT INTO users VALUES (4, "D", 25);
--- INSERT INTO users VALUES (5, "E", 25);
--- INSERT INTO users VALUES (6, "F", 25);
--- INSERT INTO users VALUES (7, "G", 25);
--- INSERT INTO users VALUES (8, "H", 25);
--- INSERT INTO users VALUES (9, "I", 25);
--- INSERT INTO users VALUES (10, "J", 25);
--- INSERT INTO users VALUES (11, "K", 25);
--- INSERT INTO users VALUES (12, "L", 25);
--- INSERT INTO users VALUES (13, "M", 25);
--- INSERT INTO users VALUES (14, "N", 25);
--- INSERT INTO users VALUES (15, "O", 25);
--- INSERT INTO users VALUES (16, "P", 25);
--- INSERT INTO users VALUES (17, "Q", 25);
--- INSERT INTO users VALUES (18, "R", 25);
--- INSERT INTO users VALUES (19, "S", 25);
--- INSERT INTO users VALUES (20, "T", 25);
---
--- INSERT INTO projects VALUES (1, "humane_society", "animal_volunteer",
--- 5,000, 1201, 0331);
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
