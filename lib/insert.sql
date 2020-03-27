e079f9fef72e241fab039a36f5c3ab89bd5ecd8a
Adam G Horn <hornadam8@gmail.com>
2020-03-27 15:51:43 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,120 @@
+INSERT INTO projects VALUES(
+  1,'proj_1','cat_1',1000,02/13/19,09/13/19);
+INSERT INTO projects VALUES(
+  2,'proj_2','cat_2',1000,01/11/20,05/30/20);
+INSERT INTO projects VALUES(
+  3,'proj_3','cat_3',1000,04/26/19,12/24/19);
+INSERT INTO projects VALUES(
+  4,'proj_4','cat_4',1000,02/15/20,07/18/20);
+INSERT INTO projects VALUES(
+  5,'proj_5','cat_5',1000,01/13/1,05/13/19);
+INSERT INTO projects VALUES(
+  6,'proj_6','cat_6',1000,02/23/19,06/05/19);
+INSERT INTO projects VALUES(
+  7,'proj_7','cat_7',1000,04/12/19,09/25/19);
+INSERT INTO projects VALUES(
+  8,'proj_8','cat_8',1000,02/11/20,08/24/20);
+INSERT INTO projects VALUES(
+  9,'proj_9','cat_9',1000,04/11/19,09/27/19);
+INSERT INTO projects VALUES(
+  10,'proj_10','cat_10',1000,05/15/19,07/15/19);
+INSERT INTO users VALUES(
+  1,'Aden',45);
+INSERT INTO users VALUES(
+  2,'Bill',52);
+INSERT INTO users VALUES(
+  3,'Carla',27);
+INSERT INTO users VALUES(
+  4,'Deandra',34);
+INSERT INTO users VALUES(
+  5,'Edgar',19);
+INSERT INTO users VALUES(
+  6,'Frank',87);
+INSERT INTO users VALUES(
+  7,'Geraldo',32);
+INSERT INTO users VALUES(
+  8,'Horatio',75);
+INSERT INTO users VALUES(
+  9,'Ingrid',18);
+INSERT INTO users VALUES(
+  10,'Jan',33);
+INSERT INTO users VALUES(
+  11,'Kevin',21);
+INSERT INTO users VALUES(
+  12,'Lois',44);
+INSERT INTO users VALUES(
+  13,'Marilyn',38);
+INSERT INTO users VALUES(
+  14,'Nolan',47);
+INSERT INTO users VALUES(
+  15,'Oscar',14);
+INSERT INTO users VALUES(
+  16,'Patricia',52);
+INSERT INTO users VALUES(
+  17,'Quentin',28);
+INSERT INTO users VALUES(
+  18,'Roland',39);
+INSERT INTO users VALUES(
+  19,'Sammie',19);
+INSERT INTO users VALUES(
+  20,'Timothy',65);
+INSERT INTO pledges VALUES(
+  1,50,15,5);
+INSERT INTO pledges VALUES(
+  2,35,2,10);
+INSERT INTO pledges VALUES(
+  3,100,9,1);
+INSERT INTO pledges VALUES(
+  4,20,14,2);
+INSERT INTO pledges VALUES(
+  5,40,4,9);
+INSERT INTO pledges VALUES(
+  6,60,7,6);
+INSERT INTO pledges VALUES(
+  7,10,20,10);
+INSERT INTO pledges VALUES(
+  8,10,20,8);
+INSERT INTO pledges VALUES(
+  9,10,20,6);
+INSERT INTO pledges VALUES(
+  10,30,3,3);
+INSERT INTO pledges VALUES(
+  11,75,14,4);
+INSERT INTO pledges VALUES(
+  12,25,16,6);
+INSERT INTO pledges VALUES(
+  13,125,1,1);
+INSERT INTO pledges VALUES(
+  14,85,19,7);
+INSERT INTO pledges VALUES(
+  15,30,12,10);
+INSERT INTO pledges VALUES(
+  16,20,17,8);
+INSERT INTO pledges VALUES(
+  17,35,6,2);
+INSERT INTO pledges VALUES(
+  18,45,5,4);
+INSERT INTO pledges VALUES(
+  19,40,18,8);
+INSERT INTO pledges VALUES(
+  20,20,19,3);
+INSERT INTO pledges VALUES(
+  21,55,2,5);
+INSERT INTO pledges VALUES(
+  22,60,8,7);
+INSERT INTO pledges VALUES(
+  23,30,7,4);
+INSERT INTO pledges VALUES(
+  24,50,10,9);
+INSERT INTO pledges VALUES(
+  25,25,17,3);
+INSERT INTO pledges VALUES(
+  26,40,15,10);
+INSERT INTO pledges VALUES(
+  27,27,16,5);
+INSERT INTO pledges VALUES(
+  28,55,12,9);
+INSERT INTO pledges VALUES(
+  29,60,11,7);
+INSERT INTO pledges VALUES(
+  30,40,9,4);
